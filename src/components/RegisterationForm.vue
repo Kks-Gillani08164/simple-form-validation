fee8ed236c73a1f5c2c7739ed9a334f7226ce454
Syed Kashif <kksgillani1@gmail.com>
2022-10-26 12:37:31 +0500

    First Stage Completed

--- src/components/RegisterationForm.vue
+++ src/components/RegisterationForm.vue
@@ -1,139 +1,39 @@
 <template>
-  <form @submit.prevent="onSubmit">
-    <!-- <div v-if="localerrors">
-      <p v-for="(error, index) in localerrors" :key="index">{{ error }}</p>
-    </div> -->
-    <legend>Personal Information</legend>
-    <fieldset>
-      <slot name="personal-information"></slot>
-    </fieldset>
-    <legend>General Information</legend>
-    <fieldset>
-      <slot name="general-information"></slot>
-    </fieldset>
+  <form>
     <slot></slot>
+    <button type="button" @click="onSubmit">Submit</button>
   </form>
 </template>
 
 <script>
 export default {
-  /* data() {
-    return {
-      localerrors: [],
-    };
-  },
-
-  computed: {
-    formData() {
-      return this.$store.getters.getFormData;
+  props: {
+    submitMethod: {
+      type: Function,
+      required: true,
     },
   },
-
   methods: {
-    required(value) {
-      const keys = Object.keys(value);
-      const result = keys.map((el) => {
-        if (value[el] === "" || value[el] === null || value[el] === undefined) {
-          return `${el} Field is Required`;
-        }
-        return true;
-      });
-
-      return result;
-    },
-
-    validName(firstName, lastName) {
-      if (typeof firstName !== "string") {
-        return "Please! Enter a valid First Name";
-      }
-      if (typeof lastName !== "string") {
-        return "Please! Enter a valid Last Name";
-      }
-      if (lastName.length < 3) {
-        return "Last Name at Least 3 Character Long";
-      }
-
-      return true;
-    },
-
-    validEmail(value) {
-      let regex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-
-      if (value.match(regex)) return true;
-      return "Please! Enter a Valid Email Address";
-    },
-
-    validPassword(password, passwordConfirm) {
-      if (password.length < 3) {
-        return "Password must be at least 4 Character Long";
-      }
-      if (password !== passwordConfirm) {
-        return "Entered Password is not the same as Confirm Password";
-      }
-      return true;
-    },
-
-    validPhone(phoneNumber) {
-      const numberWithoutSpace = phoneNumber.replace(/\s+/g, "");
-      if (isNaN(numberWithoutSpace) || numberWithoutSpace.length !== 11) {
-        return "Please! Enter a Valid Phone Number";
-      }
-      return true;
-    },
-
     onSubmit() {
-      const providedInputs = this.required(this.formData);
-      const checkName = this.validName(
-        this.formData.firstName,
-        this.formData.lastName
-      );
-      const emailCheck = this.validEmail(this.formData.email);
-      const checkPassword = this.validPassword(
-        this.formData.password,
-        this.formData.confirmPassword
-      );
-      const checkPhone = this.validPhone(this.formData.phone);
-
-      this.localerrors = [];
-      this.localerrors.push(
-        ...[
-          ...providedInputs,
-          checkName,
-          emailCheck,
-          checkPassword,
-          checkPhone,
-        ].filter((el) => el !== true)
-      );
-
-      if (this.localerrors.length > 0) {
-        this.$store.commit("SET_ERRORS", this.localerrors);
-      }
-
-      if (this.localerrors.every((el) => el === true)) {
-        this.$store.commit("UPDATE_FORM_DATA", this.formData);
-        this.$store.commit("REMOVE_ERRORS");
-        alert("Form Submitted Successfully");
-      }
+      this.submitMethod();
     },
-  }, */
+  },
 };
 </script>
-
-<style scoped>
-p {
-  color: red;
-  margin-bottom: 0.5rem;
-}
-form {
-  width: 27rem;
-}
-legend {
-  margin-bottom: 0.5rem;
+<style>
+button {
+  display: block;
+  margin: 0 auto;
+  padding: 1rem 2rem;
+  text-transform: uppercase;
   font-weight: bold;
-}
-fieldset {
-  padding: 1rem;
+  cursor: pointer;
   margin-bottom: 2rem;
+  background-color: blueviolet;
+  border: none;
+  box-shadow: rgba(17, 17, 26, 0.05) 0px 4px 16px,
+    rgba(17, 17, 26, 0.05) 0px 8px 32px;
+  color: #fff;
   border-radius: 0.3rem;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,72 +1,115 @@
 <template>
   <div class="home">
-    <RegistrationForm>
-      <template #personal-information>
+    <RegistrationForm :submitMethod="validate">
+      <legend>General Information</legend>
+      <fieldset>
         <div class="input__container">
           <label for="firstname">First Name </label>
           <input
+            v-model="this.formData.firstName"
             type="text"
             id="firstname"
             name="firstname"
             placeholder="First Name"
           />
+          <div v-if="errors" class="errors-container">
+            <p v-for="(error, index) in errors" :key="index">
+              {{ error.firstName }}
+            </p>
+          </div>
         </div>
         <div class="input__container">
           <label for="lastname">Last Name </label>
           <input
+            v-model="this.formData.lastName"
             type="text"
             id="lastname"
             name="lastname"
             placeholder="Last Name"
           />
+          <div v-if="errors" class="errors-container">
+            <p v-for="(error, index) in errors" :key="index">
+              {{ error.lastName }}
+            </p>
+          </div>
         </div>
         <div class="input__container">
           <label for="email">Email Address </label>
           <input
+            v-model="this.formData.email"
             type="email"
             id="email"
             name="email"
             placeholder="Email Address"
           />
+          <div v-if="errors" class="errors-container">
+            <p v-for="(error, index) in errors" :key="index">
+              {{ error.email }}
+            </p>
+          </div>
         </div>
         <div class="input__container">
           <label for="password">Password </label>
           <input
+            v-model="this.formData.password"
             type="password"
             id="password"
             name="password"
             placeholder="Password"
           />
+          <div v-if="errors" class="errors-container">
+            <p v-for="(error, index) in errors" :key="index">
+              {{ error.password }}
+            </p>
+          </div>
         </div>
         <div class="input__container">
           <label for="confirm-password">Confirm Password </label>
           <input
+            v-model="this.formData.confirmPassword"
             type="password"
             id="confirm-password"
             name="confirm-password"
             placeholder="Confirm Password"
           />
+          <div v-if="errors" class="errors-container">
+            <p v-for="(error, index) in errors" :key="index">
+              {{ error.confirmPassword }}
+            </p>
+          </div>
         </div>
         <div class="input__container">
           <label for="phone">Mobile Number </label>
           <input
+            v-model="this.formData.phone"
             type="tel"
             id="phone"
             name="phone"
             placeholder="Mobile Number"
           />
+          <div v-if="errors" class="errors-container">
+            <p v-for="(error, index) in errors" :key="index">
+              {{ error.phone }}
+            </p>
+          </div>
         </div>
-      </template>
-      <template #general-information>
+      </fieldset>
+      <legend>Personal Information</legend>
+      <fieldset>
         <div class="input__container input__container--married">
           <label for="married">Married </label>
           <input
-            v-model="formData.married"
+            v-model="this.formData.married"
             type="checkbox"
             id="married"
             name="married"
             placeholder="Married"
           />
+          <div v-if="errors" class="errors-container">
+            <p v-for="(error, index) in errors" :key="index">
+              {{ error.married }}
+            </p>
+          </div>
         </div>
         <div class="input__container input__container--gender">
           <input
@@ -74,7 +117,7 @@
             id="male"
             name="gender"
             value="male"
-            v-model="formData.gender"
+            v-model="this.formData.gender"
           />
           <label for="male">Male </label>
         </div>
@@ -84,13 +127,16 @@
             id="female"
             name="gender"
             value="female"
-            v-model="formData.gender"
+            v-model="this.formData.gender"
           />
           <label for="female">Female </label>
+          <div v-if="errors" class="errors-container">
+            <p v-for="(error, index) in errors" :key="index">
+              {{ error.gender }}
+            </p>
+          </div>
         </div>
-      </template>
-
-      <button type="submit">Submit</button>
+      </fieldset>
     </RegistrationForm>
   </div>
 </template>
@@ -101,18 +147,119 @@ import RegistrationForm from "@/components/RegisterationForm.vue";
 
 export default {
   name: "HomeView",
+  data() {
+    return {
+      formData: {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+        phone: "",
+        married: false,
+        gender: null,
+      },
+      errors: [],
+    };
+  },
+  methods: {
+    validate() {
+      let errors = [];
+      const keys = Object.keys(this.formData);
+      // Checking Required Fields are present or Not
+      errors.push(
+        ...keys.map((el) => {
+          if (
+            this.formData[el] === "" ||
+            this.formData[el] === null ||
+            this.formData[el] === undefined
+          ) {
+            const fieldName = el
+              .replace(/([a-z0-9])([A-Z])/g, "$1 $2")
+              .split(" ");
+            const capitalizeFieldName = `${
+              fieldName[0].charAt(0).toUpperCase() + fieldName[0].slice(1)
+            }${fieldName[1] ? ` ${fieldName[1]}` : ""}`;
+            return { [el]: `${capitalizeFieldName} is required` };
+          }
+          return true;
+        })
+      );
+
+      // Validating if the name fields are valid
+      if (typeof this.formData.firstName !== "string") {
+        errors.push({ firstName: "Please! Enter a valid First Name" });
+      }
+      if (typeof this.formData.lastName !== "string") {
+        errors.push({ lastName: "Please! Enter a valid Last Name" });
+      }
+      if (this.formData.lastName.length < 3) {
+        errors.push({ lastName: "Last Name at Least 3 Character Long" });
+      }
+
+      // Validating Email Address
+      let regex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+
+      if (this.formData.email.match(regex)) errors.push(true);
+      else {
+        errors.push({ email: "Please! Enter a Valid Email Address" });
+      }
+
+      // Validating Passwords
+      if (this.formData.password.length < 3) {
+        errors.push({ password: "Password must be at least 4 Character Long" });
+      }
+      if (this.formData.password !== this.formData.confirmPassword) {
+        errors.push({
+          confirmPassword:
+            "Entered Password is not the same as Confirm Password",
+        });
+      }
+
+      // Validating Phone Number
+      const numberWithoutSpace = this.formData.phone.replace(/\s+/g, "");
+      if (isNaN(numberWithoutSpace) || numberWithoutSpace.length !== 11) {
+        errors.push({ phone: "Please! Enter a Valid Phone Number" });
+      }
+
+      const localErrors = errors.filter((el) => el !== true);
+      errors = [];
+
+      this.errors = [];
+      this.errors.push(...localErrors);
+
+      if (localErrors.every((el) => el === true)) {
+        console.log("Form Submitted Successfully");
+      }
+    },
+  },
   components: {
     RegistrationForm,
-  },
-  computed: {
-    formData() {
-      return this.$store.getters.getFormData;
-    },
   },
 };
 </script>
 
 <style scoped>
+.errors-container {
+  margin-top: -0.1rem;
+}
+p {
+  color: red;
+  margin-bottom: 0.5rem;
+}
+form {
+  width: 27rem;
+}
+legend {
+  margin-bottom: 0.5rem;
+  font-weight: bold;
+}
+fieldset {
+  padding: 1rem;
+  margin-bottom: 2rem;
+  border-radius: 0.3rem;
+}
+
 label {
   font-weight: bold;
 }
@@ -138,21 +285,5 @@ input[type="email"] {
 
 input[type="radio"] {
   margin-right: 0.5rem;
-}
-
-button {
-  display: block;
-  margin: 0 auto;
-  padding: 1rem 2rem;
-  text-transform: uppercase;
-  font-weight: bold;
-  cursor: pointer;
-  margin-bottom: 2rem;
-  background-color: blueviolet;
-  border: none;
-  box-shadow: rgba(17, 17, 26, 0.05) 0px 4px 16px,
-    rgba(17, 17, 26, 0.05) 0px 8px 32px;
-  color: #fff;
-  border-radius: 0.3rem;
 }
 </style>
