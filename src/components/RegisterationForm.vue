e76aa7abbda16bb3f82be306c98e6e945fb4c019
Syed Kashif <kksgillani1@gmail.com>
2022-10-26 09:22:45 +0500

    Forms Design is reimplemented using slots

--- src/components/RegisterationForm.vue
+++ src/components/RegisterationForm.vue
@@ -1,111 +1,23 @@
 <template>
   <form @submit.prevent="onSubmit">
-    <div v-if="localerrors">
+    <!-- <div v-if="localerrors">
       <p v-for="(error, index) in localerrors" :key="index">{{ error }}</p>
-    </div>
+    </div> -->
     <legend>Personal Information</legend>
     <fieldset>
-      <div class="input__container">
-        <label for="firstname">First Name </label>
-        <input
-          v-model="formData.firstName"
-          type="text"
-          id="firstname"
-          name="firstname"
-          placeholder="First Name"
-        />
-      </div>
-      <div class="input__container">
-        <label for="lastname">Last Name </label>
-        <input
-          v-model="formData.lastName"
-          type="text"
-          id="lastname"
-          name="lastname"
-          placeholder="Last Name"
-        />
-      </div>
-      <div class="input__container">
-        <label for="email">Email Address </label>
-        <input
-          v-model="formData.email"
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Email Address"
-        />
-      </div>
-      <div class="input__container">
-        <label for="password">Password </label>
-        <input
-          v-model="formData.password"
-          type="password"
-          id="password"
-          name="password"
-          placeholder="Password"
-        />
-      </div>
-      <div class="input__container">
-        <label for="confirm-password">Confirm Password </label>
-        <input
-          v-model="formData.confirmPassword"
-          type="password"
-          id="confirm-password"
-          name="confirm-password"
-          placeholder="Confirm Password"
-        />
-      </div>
-      <div class="input__container">
-        <label for="phone">Mobile Number </label>
-        <input
-          v-model="formData.phone"
-          type="tel"
-          id="phone"
-          name="phone"
-          placeholder="Mobile Number"
-        />
-      </div>
+      <slot name="personal-information"></slot>
     </fieldset>
     <legend>General Information</legend>
     <fieldset>
-      <div class="input__container input__container--married">
-        <label for="married">Married </label>
-        <input
-          v-model="formData.married"
-          type="checkbox"
-          id="married"
-          name="married"
-          placeholder="Married"
-        />
-      </div>
-      <div class="input__container input__container--gender">
-        <input
-          type="radio"
-          id="male"
-          name="gender"
-          value="male"
-          v-model="formData.gender"
-        />
-        <label for="male">Male </label>
-      </div>
-      <div class="input__container input__container--gender">
-        <input
-          type="radio"
-          id="female"
-          name="gender"
-          value="female"
-          v-model="formData.gender"
-        />
-        <label for="female">Female </label>
-      </div>
+      <slot name="general-information"></slot>
     </fieldset>
-    <button type="submit">Submit</button>
+    <slot></slot>
   </form>
 </template>
 
 <script>
 export default {
-  data() {
+  /* data() {
     return {
       localerrors: [],
     };
@@ -203,7 +115,7 @@ export default {
         alert("Form Submitted Successfully");
       }
     },
-  },
+  }, */
 };
 </script>
 
@@ -222,49 +134,6 @@ legend {
 fieldset {
   padding: 1rem;
   margin-bottom: 2rem;
-  border-radius: 0.3rem;
-}
-
-label {
-  font-weight: bold;
-}
-
-input[type="text"],
-input[type="password"],
-input[type="tel"],
-input[type="email"] {
-  padding: 0.5rem 0.3rem;
-  display: block;
-  width: 100%;
-  margin-bottom: 1rem;
-  margin-top: 0.3rem;
-}
-
-.input__container--married {
-  margin-bottom: 1rem;
-}
-
-.input__container--gender {
-  margin-bottom: 0.5rem;
-}
-
-input[type="radio"] {
-  margin-right: 0.5rem;
-}
-
-button {
-  display: block;
-  margin: 0 auto;
-  padding: 1rem 2rem;
-  text-transform: uppercase;
-  font-weight: bold;
-  cursor: pointer;
-  margin-bottom: 2rem;
-  background-color: blueviolet;
-  border: none;
-  box-shadow: rgba(17, 17, 26, 0.05) 0px 4px 16px,
-    rgba(17, 17, 26, 0.05) 0px 8px 32px;
-  color: #fff;
   border-radius: 0.3rem;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,97 @@
 <template>
   <div class="home">
-    <RegistrationForm />
+    <RegistrationForm>
+      <template #personal-information>
+        <div class="input__container">
+          <label for="firstname">First Name </label>
+          <input
+            type="text"
+            id="firstname"
+            name="firstname"
+            placeholder="First Name"
+          />
+        </div>
+        <div class="input__container">
+          <label for="lastname">Last Name </label>
+          <input
+            type="text"
+            id="lastname"
+            name="lastname"
+            placeholder="Last Name"
+          />
+        </div>
+        <div class="input__container">
+          <label for="email">Email Address </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email Address"
+          />
+        </div>
+        <div class="input__container">
+          <label for="password">Password </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+          />
+        </div>
+        <div class="input__container">
+          <label for="confirm-password">Confirm Password </label>
+          <input
+            type="password"
+            id="confirm-password"
+            name="confirm-password"
+            placeholder="Confirm Password"
+          />
+        </div>
+        <div class="input__container">
+          <label for="phone">Mobile Number </label>
+          <input
+            type="tel"
+            id="phone"
+            name="phone"
+            placeholder="Mobile Number"
+          />
+        </div>
+      </template>
+      <template #general-information>
+        <div class="input__container input__container--married">
+          <label for="married">Married </label>
+          <input
+            v-model="formData.married"
+            type="checkbox"
+            id="married"
+            name="married"
+            placeholder="Married"
+          />
+        </div>
+        <div class="input__container input__container--gender">
+          <input
+            type="radio"
+            id="male"
+            name="gender"
+            value="male"
+            v-model="formData.gender"
+          />
+          <label for="male">Male </label>
+        </div>
+        <div class="input__container input__container--gender">
+          <input
+            type="radio"
+            id="female"
+            name="gender"
+            value="female"
+            v-model="formData.gender"
+          />
+          <label for="female">Female </label>
+        </div>
+      </template>
+
+      <button type="submit">Submit</button>
+    </RegistrationForm>
   </div>
 </template>
 
@@ -13,5 +104,55 @@ export default {
   components: {
     RegistrationForm,
   },
+  computed: {
+    formData() {
+      return this.$store.getters.getFormData;
+    },
+  },
 };
 </script>
+
+<style scoped>
+label {
+  font-weight: bold;
+}
+
+input[type="text"],
+input[type="password"],
+input[type="tel"],
+input[type="email"] {
+  padding: 0.5rem 0.3rem;
+  display: block;
+  width: 100%;
+  margin-bottom: 1rem;
+  margin-top: 0.3rem;
+}
+
+.input__container--married {
+  margin-bottom: 1rem;
+}
+
+.input__container--gender {
+  margin-bottom: 0.5rem;
+}
+
+input[type="radio"] {
+  margin-right: 0.5rem;
+}
+
+button {
+  display: block;
+  margin: 0 auto;
+  padding: 1rem 2rem;
+  text-transform: uppercase;
+  font-weight: bold;
+  cursor: pointer;
+  margin-bottom: 2rem;
+  background-color: blueviolet;
+  border: none;
+  box-shadow: rgba(17, 17, 26, 0.05) 0px 4px 16px,
+    rgba(17, 17, 26, 0.05) 0px 8px 32px;
+  color: #fff;
+  border-radius: 0.3rem;
+}
+</style>
